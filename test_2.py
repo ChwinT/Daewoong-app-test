90027300c3359fb50a1315e41dbb2518548cc0ae
ChwinT <138563647+ChwinT@users.noreply.github.com>
2024-04-10 11:31:57 +0700

    Delete test_2.py

--- test_2.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-st.write('Hello world')
-df = pd.read_excel('IQVIA_Thailand total market_2024.02.01.xlsx')
-df.columns = ['country','sector','prescription','manufacturer','brand','drug','su2020','usd2020','su2021','usd2021','su2022','usd2022']
-st.dataframe(df)
