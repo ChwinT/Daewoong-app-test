b1d83780a5384b9b29f0de12fefe5c71a76610c6
ChwinT <138563647+ChwinT@users.noreply.github.com>
2024-04-10 11:00:49 +0700

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -1,4 +1,8 @@
 import streamlit as st
 import pandas as pd
+from plotly import express as px
 st.write('Hello world')
-st.dataframe(pd.DataFrame({'index':[1,2,3],'value':[58,98,43]}))
+df = pd.read_excel('IQVIA_Thailand total market_2024.02.01.xlsx')
+df.columns = ['country','sector','prescription','manufacturer','brand','drug','su2020','usd2020','su2021','usd2021','su2022','usd2022']
+st.dataframe(df)
+st.plotly_chart(px.scatter(df,x='su2022',y='usd2022'))
